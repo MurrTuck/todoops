6d2b2c33dfac3f34f9bc34c52ecbd810b392aaae
MurrTuck <murrtuck@gmail.com>
2019-01-27 16:13:37 -0700

    Introduced State with dummy data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
 
+import './App.css';
+
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Wash dishes',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Finish To Do App',
+        completed: false
+      },
+    ]
+  }
+
   render() {
     return (
       <div className="App">
