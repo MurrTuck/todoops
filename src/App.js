752768317e1471b20e09090b390b4b2cc7f62492
MurrTuck <murrtuck@gmail.com>
2019-01-28 22:26:51 -0700

    Setting State

--- src/App.js
+++ src/App.js
@@ -29,8 +29,16 @@ class App extends Component {
     ]
   }
 
+  // Toggle Complete
   markComplete = (id) => {
-
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    })
   }
 
   render() {
