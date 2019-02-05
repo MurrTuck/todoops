d4edec965fe8e5cd7d038f68c75b7234af0cb129
MurrTuck <murrtuck@gmail.com>
2019-02-05 10:52:32 -0700

    Updating Delete To-Do

--- src/App.js
+++ src/App.js
@@ -34,9 +34,9 @@ class App extends Component {
 
   // Delete Todo
   delTodo = (id) => {
-    this.setState({
+    axios.delete('https://jsonplaceholder.typicode.com/todos/${id}').then(res => this.setState({
       todos: [...this.state.todos.filter(todo => todo.id !== id)]
-    })
+    }))
   }
 
   // Add To-Do
